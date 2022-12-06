d4928b987995f35ea7a0db99484ff413fed02fc5
Glen Cooper <glen@glencooper.com>
2022-12-06 11:51:44 +0000

    Update glenbot2setup.sh

--- glenbot2setup.sh
+++ glenbot2setup.sh
@@ -36,6 +36,7 @@ if [ -f $glenbot2filename ]
 then
   echo "20221206T113729Z: ERROR!  Filename \"$glenbot2filename\" already exists!"
 else
+  echo "Appending to filename \"$glenbot2filename\"..."
   echo "alias c='clear'" >> $glenbot2filename
 fi
   
