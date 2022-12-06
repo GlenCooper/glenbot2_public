2f16f22b8e653741b8d074e976065757c96d2d60
Glen Cooper <glen@glencooper.com>
2022-12-06 10:40:10 +0000

    Upload glenbot2setup.sh

---
+++ glenbot2setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 20221206T072650Z: I got tired of creating new linux boxes for various things
+# and having to set up each one to my preferences.  This script will automate
+# that process.
+
+# This script should be run everywhere, and self smart enough to be run more
+# than once and not consume more resources whenever doing so.
+
+# colors as learned from https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+#echo "${red}red text ${green}green text${reset}"
+
+# to see if GlenBot2 is installed, check for a line about ~/.glenbot2_aliases
+if grep -Pq "if \[ -f ~/.glenbot2_aliases \]; then" ~/.bashrc
+then
+  tput setaf 2 # set output to green
+  /usr/bin/printf "\xE2\x9C\x94" # print a checkmark
+  tput sgr0  # reset output
+  /usr/bin/printf " GlenBot2 is installed.\n"
+else
+  echo -n "20221206T090649Z: GlenBot2 is installing itsef here... "
+  echo "if [ -f ~/.glenbot2_aliases ]; then" >> ~/.bashrc
+  echo "    . ~/.glenbot2_aliases" >> ~/.bashrc
+  echo "fi" >> ~/.bashrc
+  # wget the setup script from this github url
+  echo "Done"
+fi
